8b9425e76d14deb1123bb19d29db86b16ff43a3f
Suresh Peiris <tsmpmail@gmail.com>
2019-11-18 00:23:13 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatTableModule, MatCardModule, MatInputModule, MatSelectModule } from '@angular/material';
+
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
+import { of } from 'rxjs';
+
+const currency: string = "LKR";
+let vehicles: any = [];
+
+export interface PeriodicElement {
+  plateNumber: string;
+  make: string;
+  model: string;
+  dayRental: string;
+  type: string
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent {
+
+  displayedColumns: string[] = ['plateNumber', 'make', 'model', 'dayRental', 'type', 'action'];
+  dataSource: PeriodicElement[] = [];
+
+
+  constructor(private httpService: HttpClient) { }
+
+  search(form) {
+    const query = form.value.query.toLowerCase();
+    const searchType = form.value.searchType;
+    let filteredArray = [];
+
+    if (query != "" && searchType != "") {
+      filteredArray = [];
+      if (searchType === "plate") {
+        filteredArray.push(vehicles.find(vehicle => vehicle.plateNumber === query));
+      } else if (searchType == "model") {
+        vehicles.forEach(vehicle => {
+          if (vehicle.model.toLowerCase() === query) {
+            filteredArray.push(vehicle);
+          }
+        });
+      } else if (searchType === "make") {
+        vehicles.forEach(vehicle => {
+          if (vehicle.make.toLowerCase() === query) {
+            filteredArray.push(vehicle);
+          }
+        });
+      } else if (searchType === "rental") {
+        vehicles.forEach(vehicle => {
+          if (vehicle.dayRental === query + " " + currency) {
+            filteredArray.push(vehicle);
+          }
+        });
+      } else if (searchType === "type") {
+        vehicles.forEach(vehicle => {
+          if (vehicle.type.toLowerCase() === query) {
+            filteredArray.push(vehicle);
+          }
+        });
+      }
+    }
+    console.log("Search result count: " + filteredArray.length);
+    this.dataSource = filteredArray;
+  }
+
+  clearSearch(form) {
+    form.controls["query"].setValue("");
+    form.controls["searchType"].setValue("plate");
+    this.dataSource = vehicles;
+  }
+
+  ngOnInit() {
+    this.httpService.get('http://localhost:3000/vehicles').subscribe(
+      data => {
+        //this.dataSource = data as PeriodicElement[];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrBirds[1]);
+        let jsonStr = JSON.stringify(data);
+        let jsonData = JSON.parse(jsonStr);
+        vehicles = jsonData.vehicles.map(vehicle => {
+          return {
+            plateNumber: vehicle.plateNo,
+            make: vehicle.make,
+            model: vehicle.model,
+            dayRental: vehicle.dayRental + " " + currency,
+            type: vehicle.type
+          };
+        });
+        this.dataSource = vehicles;
+        console.log(vehicles);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
+  }
+
+}
